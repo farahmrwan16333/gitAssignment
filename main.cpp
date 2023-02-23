ceb82b947c464cc42a965b9e73aefa29799aa1a5
caty caty <catherinekaram11@gmail.com>
2023-02-23 13:27:18 +0000

    added getAverage function

--- main.cpp
+++ main.cpp
@@ -8,8 +8,14 @@ int getSum(std::vector<int> numbers){
     }
     return sum;
 }
+float getAverage (std::vector <int> numbers)
+{
+return ((float)getSum(numbers) / numbers.size());}
 
 int main(){
     std::vector<int> numbers={1,2,3,4,5,6,7,8,9,10};
-    std::cout<<getSum(numbers);
+    std::cout<<"Sum is "<<getSum(numbers)<<"\n";
+	std::cout<<"Average is "<<getAverage(numbers)<<"\n";
 }
+
+
